58a807c3b9b0b885225a06e9518f7e15f0332eb8
Ayoub Ben zaira <benzaira.ayoub@gmail.com>
2023-12-31 18:53:41 +0100

    Implementing Cross-Origin Resource Sharing config (CORS) with Spring security 6.1.1

--- src/main/java/com/example/bankbackendsecurityapp/config/SecurityConfig.java
+++ src/main/java/com/example/bankbackendsecurityapp/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.bankbackendsecurityapp.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -14,18 +15,34 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
 
-    //Simple filters with basic auth
-
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
         @Bean
         SecurityFilterChain defaultSercurityFilterChain(HttpSecurity http) throws Exception {
-            http.csrf(AbstractHttpConfigurer::disable)
+            http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                    .csrf(AbstractHttpConfigurer::disable)
                     .authorizeHttpRequests(authorize -> authorize
                             .requestMatchers("/myAccount", "/myBalance", "/myLoans", "myCards")
                             .authenticated()
